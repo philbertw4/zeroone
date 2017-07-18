8053b5a62c26c916d0ec4134e99922af44d039d8
philbertw4 <philbertw4@users.noreply.github.com>
2017-07-17 19:06:23 -0700

    more monadic design, still trying to get simplification to work (but probably running into an aspect of Rice's theorem)

--- src/main/scala/zeroone/ZeroOne.scala
+++ src/main/scala/zeroone/ZeroOne.scala
@@ -1,6 +1,6 @@
 package zeroone
 
-import scala.util.Try
+import scala.util.{Try,Success,Failure}
 import scala.language.implicitConversions
 
 sealed trait ZeroOne {
@@ -27,6 +27,7 @@ object ZeroOne {
   }
   sealed trait Data extends ZeroOne {
     val elems: List[Quoted]
+    override def equals(o: Any) = o match {case d: Data => d.elems == elems; case _ => false}
   }
   object Data {
     final case class Input(elems: List[Quoted]) extends Data
@@ -45,52 +46,85 @@ object ZeroOne {
     def apply(elems: Quoted*): Input = Input(elems.toList)
   }
 
-  final case class StackEffect(action: Data.Input => Data.Output, name: String = "") extends ZeroOne
+  final case class StackEffect(action: Data => Either[String,Data], name: String = "") extends ZeroOne
+            object StackEffect {
+              def apply(name: String)(action: Data => Either[String,Data]):StackEffect = new StackEffect(action,name)
+            }
+
+  // some implicit conversions to make our lives easier
+  implicit def tryToEitherStringT[T](t: Try[T]): Either[String,T] = t match {case Success(r) => Right(r); case Failure(e) => Left(e.getMessage)}
+  implicit def eitherStringTtoTry[T](either: Either[String,T]): Try[T] = either match { case Right(v) => Success(v); case Left(s) => Failure(new RuntimeException(s))}
+
 
   // these combinators nothing "no operation" -- helpful for testing though
-  val NOOP1: StackEffect = StackEffect((input) => input, "NOOP1")
-  val NOOP2: StackEffect = StackEffect((input) => input, "NOOP2")
-  val NOOP3: StackEffect = StackEffect((input) => input, "NOOP3")
-  val NOOP4: StackEffect = StackEffect((input) => input, "NOOP4")
-  val NOOP5: StackEffect = StackEffect((input) => input, "NOOP5")
-  val NOOP6: StackEffect = StackEffect((input) => input, "NOOP6")
+  val NOOP1: StackEffect = StackEffect((input) => Right(input), "NOOP1")
+  val NOOP2: StackEffect = StackEffect((input) => Right(input), "NOOP2")
+  val NOOP3: StackEffect = StackEffect((input) => Right(input), "NOOP3")
+  val NOOP4: StackEffect = StackEffect((input) => Right(input), "NOOP4")
+  val NOOP5: StackEffect = StackEffect((input) => Right(input), "NOOP5")
+  val NOOP6: StackEffect = StackEffect((input) => Right(input), "NOOP6")
 
   // the empty quotation, as a combinator
-  val nil: StackEffect = StackEffect((input) => Data.Output(Quoted.empty :: input.elems), "OP_NIL")
+  val nil: StackEffect = StackEffect((input) => Right(Data.Output(Quoted.empty :: input.elems)), "OP_NIL")
 
-  val zap: StackEffect = StackEffect((input) => Data.Output(input.elems.tail), "OP_ZAP")
+  val zap: StackEffect = StackEffect((input) => Try(input.elems.tail) match {case Success(r) => Right(Data.Output(r)); case Failure(e) => Left(e.getMessage)}, "OP_ZAP")
+
   val swap: StackEffect = {
-    val action: Data.Input => Data.Output = input => {
-      val a = input.elems.head
-      val b = input.elems.drop(1).head
-      Data.Output(b :: a :: input.elems.drop(1).tail)
+    val action: Data => Either[String,Data] = input => {
+      val result = for(
+        a <- Try(input.elems.head);
+        b <- Try(input.elems.drop(1).head)
+      ) yield Data.Output(b :: a :: input.elems.drop(1).tail)
+      result match {
+        case Success(r) => Right(r)
+        case Failure(e) => Left(e.getMessage)
+      }
     }
     StackEffect(action, "OP_SWAP")
   }
 
   val cons: StackEffect = {
-    val action: Data.Input => Data.Output = input => {
-      val a = input.elems.head.program
-      val b = input.elems.drop(1).head.program
-      Data.Output(Quoted(Program(Quoted(b) :: a.elems)) :: input.elems.drop(1).tail)
+    val action: Data => Either[String,Data] = input => {
+      (for( a <- Try(input.elems.head.program);
+           b <- Try(input.elems.drop(1).head.program)
+      ) yield Data.Output(Quoted(Program(Quoted(b) :: a.elems)) :: input.elems.drop(1).tail)) match {
+        case Success(r) => Right(r)
+        case Failure(e) => Left(e.getMessage)
+      }
     }
     StackEffect(action, "OP_CONS")
   }
   import eval._
-  val i: StackEffect = StackEffect((input) => input.elems.head.program.eval(Data.Input(input.elems.tail)), "OP_I")
+
+  val i = StackEffect("OP_I"){
+    input => Try(input.elems.head.program).flatMap(_.eval(Data.Input(input.elems.drop(1))))
+  }
+
 
   val sap: StackEffect = {
-    val action: Data.Input => Data.Output = input => {
-      val a = input.elems.head.program
-      val b = input.elems.drop(1).head.program
-      b.eval(a.eval(Data.Input(input.elems.drop(2))))
+    val action: Data => Either[String, Data] = input => {
+      for( a <- Try(input.elems.head.program);
+           b <- Try(input.elems.drop(1).head.program);
+          result <- a.eval(Data.Input(input.elems.drop(2))).flatMap(b.eval(_))
+      ) yield result
     }
     StackEffect(action, "OP_SAP")
   }
 
-  val k: StackEffect = StackEffect((input) => input.elems.head.program.eval(Data.Input(input.elems.drop(2))), "OP_K")
+  val k: StackEffect = StackEffect(
+    (input) => input.elems.headOption match {
+    case Some(q) => q.program.eval(Data.Input(input.elems.drop(2)))
+    case None => Left("stack underflow when trying to evaluate k combinator")
+  }, "OP_K")
+
   val R: StackEffect = StackEffect(k.action, "OP_R") //R is just a synonym for k
-  val z: StackEffect = StackEffect((input) => input.elems.drop(1).head.program.eval(Data.Input(input.elems.drop(2))), "OP_Z")
+
+  val z: StackEffect = StackEffect(
+    (input) => input.elems.drop(1).headOption match {
+      case Some(q) => q.program.eval(Data.Input(input.elems.drop(2)))
+      case None => Left("stack underflow when trying to evaluate z combinator")
+    }, "OP_Z")
+
   val L: StackEffect = StackEffect(z.action, "OP_L") //L is just a synonym for z
 
   //some sample symbol mappings
@@ -111,25 +145,71 @@ object ZeroOne {
   }
 }
 
+object eval {
+  import ZeroOne._
+
+  implicit class ImplZeroOneAsData(stack: List[ZeroOne]) {
+    def asData: Try[Data.Input] = stack.forall(_.isInstanceOf[Quoted]) match {
+      case true => Success(Data.Input(stack.map(_.asInstanceOf[Quoted])))
+      case false => Failure( new Exception("unable to convert stack to data!"))
+    }
+  }
+
+  implicit class ImplEvals(p: ZeroOne) {
+    def eval(inputStack: Data = Data.empty): Either[String,Data] = p match {
+      case Program(elems) => elems.foldLeft(Right(inputStack).asInstanceOf[Either[String,Data]]) {  //eval child elements
+        case (Right(stack), elem) => elem.eval(stack)
+        case (Left(error), elem) => Left(error + elem.toString) //propagate the error
+      }
+      case quotedProg: Quoted => Right(Data.Output(quotedProg :: inputStack.elems)) //just push quote onto the stack
+      case StackEffect(action,_) => action(inputStack) //pass the stack to the action thereby evaluating it
+      case Data.Input(elems) => Right(Data.Output(elems ++ inputStack.elems))//elems.foldLeft(inputStack) {case (stack, elem) => elem.eval(stack)}
+      case Data.Output(elems) => Right(Data.Output(elems ++ inputStack.elems)) //elems.foldLeft(inputStack) {case (stack, elem) => elem.eval(stack)}
+    }
+  }
+
+  implicit class ImplProgramOps(p: Program) {
+    def simplify: Program = Program(eval.simplify(p.elems))
+  }
+  def simplify(elems: List[ZeroOne]): List[ZeroOne] = {
+    println(s"simplifying program $elems")
+    elems.foldLeft((List.empty[ZeroOne],elems)) {
+      case ((stack,continuation), elem: Quoted) =>
+        println(s" adding quoted item $elem to stack")
+        (elem :: stack, continuation.drop(1))
+      case ((stack, continuation), elem: Program) =>
+        println(s" simplifying sub program $elem")
+        (simplify((elem.elems.reverse ++ stack).reverse), continuation.drop(1))
+      case ((stack, continuation), elem: StackEffect) => {
+        println(s" simplifying $elem")
+        val data = stack.asData
+        println(s" stack is $stack, data is $data")
+        data.map(d => elem.action(d)) match {
+          case Success(Right(output)) =>
+            println("success rewriting $elem")
+            //Program(output.elems.reverse)
+            (output.elems, continuation.drop(1))
+          case Success(Left(msg)) =>
+            println(s"Left($msg)")
+            (continuation.drop(1)++(elem :: stack), continuation.drop(1))
+          case _ =>
+            println("failed, so returning stack so far plus program")
+            (continuation.drop(1)++(elem :: stack), continuation.drop(1))
+        }
+      }
+      case ((stack,continuation),_) =>
+        println("OTHER")
+        (stack,continuation.drop(1))
+    }._1.reverse
+  }
+
+}
+
 object parse {
   import ZeroOne._
   def apply(raw: String)(implicit symbolMapping: Map[Char,Program]): Program = raw.map(c=>symbolMapping.get(c)).foldLeft(Program.empty){
     case (Program(elems), Some(p)) => Program(elems ++ p.elems)
     case (Program(elems), _) => Program(elems) //unknown character so ignore and move on
-  }
-}
-
-object eval {
-  import ZeroOne._
-
-  implicit class ImplEvals(p: ZeroOne) {
-    def eval(inputStack: Data.Input = Data.empty): Data.Output = p match {
-      case Program(elems) => elems.foldLeft(inputStack) {case (stack, elem) => elem.eval(stack)} //eval child elements
-      case quotedProg: Quoted => Data.Output(quotedProg :: inputStack.elems) //just push quote onto the stack
-      case StackEffect(action,_) => action(inputStack) //pass the stack to the action thereby evaluating it
-      case Data.Input(elems) => elems.foldLeft(inputStack) {case (stack, elem) => elem.eval(stack)}
-      case Data.Output(elems) => elems.foldLeft(inputStack) {case (stack, elem) => elem.eval(stack)}
-    }
   }
 }
 
--- src/test/scala/zeroone/ZeroOneSuite.scala
+++ src/test/scala/zeroone/ZeroOneSuite.scala
@@ -19,7 +19,15 @@ class ParseSuite extends FunSuite {
   }
   test("parse and eval 0 01 == [ [[cons] [sap]] [[nil] [swap]] ]") {
     import eval._
-    assert(parse("0 01").eval(Data.empty) == Data.Output( Quoted(Quoted(nil),Quoted(swap)), Quoted(Quoted(cons),Quoted(sap))) )
+    assert(parse("0 01").eval(Data.empty) == Right(Data.Output( Quoted(Quoted(nil),Quoted(swap)), Quoted(Quoted(cons),Quoted(sap)))) )
+  }
+  test("parse and simplify 1 == R") {
+    import eval._
+    assert( parse("1").simplify == Program(R))
+  }
+  test("parse and symplify 01 == L ") {
+    import eval._
+    assert( parse("01").simplify == Program(L))
   }
 
 }
@@ -43,46 +51,46 @@ class PrintSuite extends FunSuite {
 class EvalSuite extends FunSuite {
   import eval._
   test("eval empty program should not change stack") {
-    assert(ZeroOne.Program.empty.eval(Data(Quoted(Program(NOOP1)), Quoted(Program(NOOP2)))) == Data.Output(Quoted(Program(NOOP1)), Quoted(Program(NOOP2))))
+    assert(ZeroOne.Program.empty.eval(Data(Quoted(Program(NOOP1)), Quoted(Program(NOOP2)))) == Right(Data.Output(Quoted(Program(NOOP1)), Quoted(Program(NOOP2)))))
   }
   test("eval nil should push empty quotation on to stack") {
-    assert(Program(nil).eval(Data.empty) == Data.Output(Quoted.empty))
+    assert(Program(nil).eval(Data.empty) == Right(Data.Output(Quoted.empty)))
   }
   test("[NOOP1] [NOOP2] NOOP1 == [NOOP1] [NOOP2]") {
-    assert( Program(Quoted(Program(NOOP1)), Quoted(Program(NOOP2)), NOOP2).eval(Data.empty) == Data.Output(Quoted(Program(NOOP2)), Quoted(Program(NOOP1))))
+    assert( Program(Quoted(Program(NOOP1)), Quoted(Program(NOOP2)), NOOP2).eval(Data.empty) == Right(Data.Output(Quoted(Program(NOOP2)), Quoted(Program(NOOP1)))))
   }
   test("eval from data") {
-    assert( Data(Quoted(NOOP1), Quoted(NOOP2)).eval(Data.empty) == Data.Output(Quoted(NOOP2),Quoted(NOOP1)))
+    assert( Data(Quoted(NOOP1), Quoted(NOOP2)).eval(Data.empty) == Right(Data.Output(Quoted(NOOP1),Quoted(NOOP2))))
   }
   test("[NOOP1] [NOOP2] [NOOP3] zap == [NOOP1] [NOOP2]") {
-    assert( Program(Quoted(NOOP1),Quoted(NOOP2), Quoted(NOOP3), zap).eval(Data.empty) == Data.Output(Quoted(NOOP2), Quoted(NOOP1)))
+    assert( Program(Quoted(NOOP1),Quoted(NOOP2), Quoted(NOOP3), zap).eval(Data.empty) == Right(Data.Output(Quoted(NOOP2), Quoted(NOOP1))))
   }
   test("[NOOP1] [NOOP2] zap [NOOP3] == [NOOP1] [NOOP3]") {
-    assert( Program(Quoted(NOOP1), Quoted(NOOP2), zap, Quoted(NOOP3)).eval(Data.empty) == Data.Output(Quoted(NOOP3), Quoted(NOOP1)))
+    assert( Program(Quoted(NOOP1), Quoted(NOOP2), zap, Quoted(NOOP3)).eval(Data.empty) == Right(Data.Output(Quoted(NOOP3), Quoted(NOOP1))))
   }
   test("[NOOP1] [NOOP2] [zap] i == [NOOP1]") {
-    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(zap), i).eval(Data.empty) == Data.Output(Quoted(NOOP1)))
-    assert( Program(Data(Quoted(NOOP1), Quoted(NOOP2), Quoted(zap)), i).eval(Data.empty) == Data.Output(Quoted(NOOP1)))
+    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(zap), i).eval(Data.empty) == Right(Data.Output(Quoted(NOOP1))))
+    assert( Program(Data(Quoted(zap), Quoted(NOOP2), Quoted(NOOP1)), i).eval(Data.empty) == Right(Data.Output(Quoted(NOOP1))))
   }
   test("[zap] [NOOP1] swap == [NOOP1] [zap]") {
-    assert( Program(Quoted(zap), Quoted(NOOP1), swap).eval(Data.empty) == Data.Output(Quoted(zap),Quoted(NOOP1)))
+    assert( Program(Quoted(zap), Quoted(NOOP1), swap).eval(Data.empty) == Right(Data.Output(Quoted(zap),Quoted(NOOP1))))
   }
   test("[NOOP1] [NOOP2] cons == [[NOOP1] NOOP2]") {
-    assert( Program(Quoted(NOOP1), Quoted(NOOP2), cons).eval(Data.empty) == Data.Output(Quoted(Quoted(NOOP1), NOOP2)))
+    assert( Program(Quoted(NOOP1), Quoted(NOOP2), cons).eval(Data.empty) == Right(Data.Output(Quoted(Quoted(NOOP1), NOOP2))))
   }
   test("[NOOP1] [NOOP2] [NOOP3] [cons] [zap] sap == [NOOP1] [NOOP2] [NOOP3] zap cons == [[NOOP1] NOOP2]") {
-    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(NOOP3), Quoted(cons), Quoted(zap), sap).eval(Data.empty) == Data.Output(Quoted(Quoted(NOOP1), NOOP2)))
+    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(NOOP3), Quoted(cons), Quoted(zap), sap).eval(Data.empty) == Right(Data.Output(Quoted(Quoted(NOOP1), NOOP2))))
   }
   test("[NOOP1] [NOOP2] [NOOP3] [zap] [cons] sap == [NOOP1] [NOOP2] [NOOP3] cons zap == [NOOP1]") {
-    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(NOOP3), Quoted(zap), Quoted(cons), sap).eval(Data.empty) == Data.Output(Quoted(NOOP1)))
+    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(NOOP3), Quoted(zap), Quoted(cons), sap).eval(Data.empty) == Right(Data.Output(Quoted(NOOP1))))
   }
   test("[NOOP1] [NOOP2] [zap] k == [NOOP1]") {
-    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(zap), k).eval(Data.empty) == Data.Output.empty)
-    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(zap), R).eval(Data.empty) == Data.Output.empty)
+    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(zap), k).eval(Data.empty) == Right(Data.Output.empty))
+    assert( Program(Quoted(NOOP1), Quoted(NOOP2), Quoted(zap), R).eval(Data.empty) == Right(Data.Output.empty))
   }
   test("[NOOP2] [NOOP3] [NOOP1] [zap] [NOOP4] z == [NOOP2] [NOOP3]") {
-    assert( Program(Quoted(NOOP2), Quoted(NOOP3), Quoted(NOOP1), Quoted(zap), Quoted(NOOP4), z).eval(Data.empty) == Data.Output(Quoted(NOOP3),Quoted(NOOP2)))
-    assert( Program(Quoted(NOOP2), Quoted(NOOP3), Quoted(NOOP1), Quoted(zap), Quoted(NOOP4), L).eval(Data.empty) == Data.Output(Quoted(NOOP3),Quoted(NOOP2)))
+    assert( Program(Quoted(NOOP2), Quoted(NOOP3), Quoted(NOOP1), Quoted(zap), Quoted(NOOP4), z).eval(Data.empty) == Right(Data.Output(Quoted(NOOP3),Quoted(NOOP2))))
+    assert( Program(Quoted(NOOP2), Quoted(NOOP3), Quoted(NOOP1), Quoted(zap), Quoted(NOOP4), L).eval(Data.empty) == Right(Data.Output(Quoted(NOOP3),Quoted(NOOP2))))
   }
 }
 
